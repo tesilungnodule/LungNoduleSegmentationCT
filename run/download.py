cb1c2625e2ff248f6e70af6f137fc56fae5c55bb
marco amati <m.amati@live.it>
2021-11-08 09:54:03 +0100

    way to adapt to lungnodulesegmentation - commented ds download 2

--- run/download.py
+++ run/download.py
@@ -10,19 +10,18 @@ import os
 
 def run(dataset_dir="datasets"):
     os.makedirs(dataset_dir, exist_ok=True)
-
+    '''
     url = 'https://drive.google.com/uc?id=1RzPB1_bqzQhlWvU-YGvZzhx2omcDh38C&export=download'
     output_tar = os.path.join(dataset_dir, 'Task04_Hippocampus.tar')
     if not os.path.exists(output_tar):
-        pass
-        #gdown.download(url, output_tar, quiet=False)
+        gdown.download(url, output_tar, quiet=False)
     else:
         print("Output tar {} already exists!".format(output_tar))
 
     tar = tarfile.open(output_tar)
     tar.extractall(path=dataset_dir)
     tar.close()
-
+    '''
 
 ############################
 # MAIN
